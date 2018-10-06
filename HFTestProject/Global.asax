d49307220a28d0f8e26212dc1ca237c766d19648
cindy <c931534@gmail.com>
2018-10-06 15:34:46 +0800

    first commit

---
+++ HFTestProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HFTestProject.MvcApplication" Language="C#" %>
---
+++ HFTestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HFTestProject.Startup))]
+namespace HFTestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
